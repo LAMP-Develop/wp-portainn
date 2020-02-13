f638b88a36d37f0e191b5a7fa422fa82b2a7e52a
ShunyaSuzuki <anyushu2017@gmail.com>
2020-02-13 17:57:57 +0900

    update

--- foot-link.php
+++ foot-link.php
@@ -5,11 +5,11 @@ $wp_url = get_template_directory_uri();
 
 <div class="foot-link">
   <div class="wrap">
-    <a href="/facilities/">
+    <a href="<?php echo $home; ?>/facilities/">
       <img src="<?php echo $wp_url; ?>/img/icon01.svg" alt="facilities">
       <span>館内のご案内</span>
     </a>
-    <a href="/access/">
+    <a href="<?php echo $home; ?>/access/">
       <img src="<?php echo $wp_url; ?>/img/icon02.svg" alt="access">
       <span>交通アクセス</span>
     </a>
