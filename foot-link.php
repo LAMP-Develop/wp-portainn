827ba681d3bec460e282bd2a70d0d9cc06385025
ShunyaSuzuki <anyushu2017@gmail.com>
2020-02-13 17:58:17 +0900

    update

--- foot-link.php
+++ foot-link.php
@@ -4,14 +4,14 @@ $wp_url = get_template_directory_uri();
 ?>
 
 <div class="foot-link">
-  <div class="wrap">
-    <a href="<?php echo $home; ?>/facilities/">
-      <img src="<?php echo $wp_url; ?>/img/icon01.svg" alt="facilities">
-      <span>館内のご案内</span>
-    </a>
-    <a href="<?php echo $home; ?>/access/">
-      <img src="<?php echo $wp_url; ?>/img/icon02.svg" alt="access">
-      <span>交通アクセス</span>
-    </a>
-  </div>
+<div class="wrap">
+<a href="<?php echo $home; ?>/facilities/">
+<img src="<?php echo $wp_url; ?>/img/icon01.svg" alt="facilities">
+<span>館内のご案内</span>
+</a>
+<a href="<?php echo $home; ?>/access/">
+<img src="<?php echo $wp_url; ?>/img/icon02.svg" alt="access">
+<span>交通アクセス</span>
+</a>
+</div>
 </div>
